d273bd4a71d769ff298345fce85c25ee761baa7f
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-23 18:02:55 +0530

    var,let,const

--- 23/05/2025/index.js
+++ 23/05/2025/index.js
@@ -2,3 +2,15 @@
 console.log(age)
 var age = 10;
 console.log(age)
+
+//var -Global
+//Re-assign
+//Kahi bhi use kar skte hain
+
+//let=locaal
+// Re-assign
+//sirf apne block m use kar skte hain
+
+//const -local
+//Re-assign nhi karskte 
+//apne block m use karskte hain
