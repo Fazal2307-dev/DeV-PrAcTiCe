83b0f80aa385801eba2a31cda3544cb1176865b7
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-23 19:15:52 +0530

    condditional statement

--- 23/05/2025/index.js
+++ 23/05/2025/index.js
@@ -1,7 +1,7 @@
 
-console.log(age)
-var age = 10;
-console.log(age)
+// console.log(age)
+// var age = 10;
+// console.log(age)
 
 //var -Global
 //Re-assign
@@ -16,8 +16,37 @@ console.log(age)
 //apne block m use karskte hain
 
 //Data types
-console.log(typeof "fazal")
-console.log("1"+"1")
-console.log(1+"1")
-console.log(10-"2")
-console.log(typeof("123" * 3))
+// console.log(typeof "fazal")
+// console.log("1"+"1")
+// console.log(1+"1")
+// console.log(10-"2")
+// console.log(typeof("123" * 3))
+// console.log("abc"*1)
+
+// conditional statement
+//if
+// const age=8;
+// if(age>=18){
+//   console.log("you can marry")
+// }else if(age<10){
+//   console.log("you r pedophile");
+// }else{
+//   console.log("you r rajisthani")
+// }
+
+//Ternary operator
+// const age = 10;
+// age >=18 ? console.log("you r valid for vote") : console.log("Wait for few years");
+
+// switch case
+// customer care
+
+const daile = 5;
+switch(daile){
+  case 1:console.log("Kiya kaam hai")
+  break;
+  case 2:console.log("why r you calling")
+  break;
+  default:console.log("invalid")
+}
+
