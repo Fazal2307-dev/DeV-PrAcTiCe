f866d064195a858db8b19418e247e34952d3c8e0
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-24 17:03:12 +0530

    guess the no

--- 24/05/2025/index.js
+++ 24/05/2025/index.js
@@ -6,14 +6,32 @@ console.log("Learning loop in js");
 // }
 
 //while
-let ip=50;
-let home = 100;
-// while(ip!=home){
-//   ip = ip +1;
-//   console.log('step count' + ip);
-// }
-// do while
+// let ip=50;
+// let home = 100;
+// // while(ip!=home){
+// //   ip = ip +1;
+// //   console.log('step count' + ip);
+// // }
+// // do while
+// do{
+//   ip = ip+1;
+//   console.log('stp count'+ ip)
+// }while(ip!=home);
+
+// guess the no
+
+let otp = 123;
+let guess = 0;
 do{
-  ip = ip+1;
-  console.log('stp count'+ ip)
-}while(ip!=home);
+  guess = parseInt(prompt("please provide valid otp"))
+    if(guess == otp){
+      alert('you submit correct otp')
+      break;
+  }
+}while(guess!=0)
+
+
+
+
+
+
