e1a2000df2c81c44c15c12fb65c817ec9d6c3e62
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-26 12:44:00 +0530

    fn return fn

--- 26/05/2025/index.js
+++ 26/05/2025/index.js
@@ -7,3 +7,13 @@ cb(result);
 }
 
 add(2,5, (val)=>console.log(val))
+
+
+// fn return fn
+function add(a,b ,cb){
+  let result = a+b;
+  cb(result);
+return ()=>console.log(result)
+}
+let sum = add(1,8,()=>{})
+sum()
