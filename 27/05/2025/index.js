88b3dab516cbe920da1d31f3e9d70cfe01108818
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-27 17:20:24 +0530

    getElementsbyclass

--- 27/05/2025/index.js
+++ 27/05/2025/index.js
@@ -6,8 +6,13 @@
 //  const body = document.querySelector('body')
 //  body.innerHTML='<h1>You are hacked!!! </h1>'
 
- const pre = document.querySelector('pre')
- pre.innerHTML = '<h2>This is change</h2>'
+//  const pre = document.querySelector('pre')
+//  pre.innerHTML = '<h2>This is change</h2>'
 
- const name1 = document.querySelector('#username')
- name1.innerHTML = '<h2>JOHN DOE</h2>'
+//  const name1 = document.querySelector('#username')
+//  name1.innerHTML = '<h2>JOHN DOE</h2>'
+
+const elements = document.getElementsByClassName('block')
+for(let i=0;i<elements.length;i++){
+  elements.item(i).style.background = "blue";
+}
