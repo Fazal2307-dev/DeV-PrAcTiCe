5de185776ab502ec73e94b78f05d917f29c9fa20
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-27 15:47:03 +0530

    query selector

--- 27/05/2025/index.js
+++ 27/05/2025/index.js
@@ -1,4 +1,7 @@
 // alert("Hey Fazal r you going to complete this course within timeline");
 
-let name = prompt("whats your name");
-document.write("hello" + name);
+// let name = prompt("whats your name");
+// document.write("hello" + name);
+
+ const body = document.querySelector('body')
+ body.innerHTML='<h1>You are hacked!!! </h1>'
